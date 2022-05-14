5f14692d97cfa326cf93639d7f205b05090a1a2c
Abdelaziz <fazouz2010@gmail.com>
2022-05-14 23:41:30 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import React from "react";
+import { nanoid } from "nanoid";
+import Split from "react-split";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
 
-function App() {
+export default function App() {
+  const [notes, setNotes] = React.useState(() => retriveNotes() || []);
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ""
+  );
+
+  //Saves notes into localstorage when ever notes state is changed.
+  React.useEffect(
+    () => localStorage.setItem("notes", JSON.stringify(notes)),
+    [notes]
+  );
+
+  //Parse and retrive notes from localstorage.
+  function retriveNotes() {
+    return JSON.parse(localStorage.getItem("notes"));
+  }
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  }
+
+  // figure out a way to move the updated notes further to be first in the notes array.
+  function updateNote(text) {
+    /* setNotes((oldNotes) =>
+      oldNotes.map((oldNote) => {
+        return oldNote.id === currentNoteId
+          ? { ...oldNote, body: text }
+          : oldNote;
+      })
+    ); */
+
+    const selectedNote = notes
+      .splice(
+        notes.findIndex((element) => element.id === currentNoteId),
+        1
+      )
+      .pop();
+
+    const updatedNote = { ...selectedNote, body: text };
+
+    setNotes((prevNotes) => [updatedNote, ...prevNotes]);
+  }
+
+  function findCurrentNote() {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
   );
 }
-
-export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles/SidebarStyle.css";
+
+export default function Sidebar(props) {
+  const noteElements = props.notes.map((note, index) => (
+    <div key={note.id}>
+      <div
+        className={`title ${
+          note.id === props.currentNote.id ? "selected-note" : ""
+        }`}
+        onClick={() => props.setCurrentNoteId(note.id)}
+      >
+        <h4 className="text-snippet">
+          {/* improve the title by capturing all markdown special characters */}
+          {note.body.split("\n")[0].replace("#", "")}
+        </h4>
+      </div>
+    </div>
+  ));
+
+  return (
+    <section className="pane sidebar">
+      <div className="sidebar--header">
+        <h3>Notes</h3>
+        <button className="new-note" onClick={props.newNote}>
+          +
+        </button>
+      </div>
+      {noteElements}
+    </section>
+  );
+}
